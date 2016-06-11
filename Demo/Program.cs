9c5a30ce1f80b662ee0c8f74bf213d189fe78387
fttl_ <fttl_@BLQW-SURFACE>
2016-06-11 12:31:27 +0800

    1.0

---
+++ Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using blqw.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Guid.NewGuid().ToString("n").ToLowerInvariant();
+            var code = string.Format(CODE, name);
+            var obj = (ITester)DyCompiler.CompileObject(code,typeof(ITester));
+            Console.WriteLine($"{nameof(name)}:{name}");
+            Console.WriteLine($"{nameof(obj.GetText)}:{obj.GetText()}");
+        }
+
+        const string CODE = @"
+class _{0} : ITester
+    {{
+        public string GetText()
+        {{
+            return ""{0}"";
+        }}
+    }}
+";
+    }
+
+    public interface ITester
+    {
+        string GetText();
+    }
+
+    
+
+}
---
+++ blqw.DyCompiler/nuget pack.bat
@@ -0,0 +1,2 @@
+nuget pack blqw.DyCompiler.csproj
+nuget push blqw.DyCompiler.nupkg
